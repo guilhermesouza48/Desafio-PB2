984807af4234e2522532a901a3aceac4f7d1cfe4
Guilherme Souza de Jesus <guilhermesouzacrs@hotmail.com>
2023-01-19 09:41:37 -0300

    Organizing images

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,21 +1,18 @@
 import "./App.css";
-// import welcome from "./components/welcome";
+import Logo from "./components/imgs/compass.png";
 
 function App() {
   return (
-    <div>
-      <section>
-        <h1>Welcome,</h1>
-        <h2>To continue browsing safely, log in to the network.</h2>
-      </section>
-      <section>
-        <input type="text" placeholder="user name" id="pessoa"></input>
-        <input type="password" placeholder="password" id="senha"></input>
-      </section>
-      <section>
-        <button type="Submit">Login</button>
-      </section>
-    </div>
+    
+      <div className="container">
+        <div className="formulario">
+          <p>teste</p>
+        </div>
+        <div className="Imagens">
+          <img id="logoimg" src={Logo} alt="Logo compass" />
+        </div>
+      </div>
+    
   );
 }
 
