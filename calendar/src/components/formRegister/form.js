ec421f5c0617297ed65308d5e2bd2e49b897e6c1
Guilherme Souza de Jesus <guilhermesouzacrs@hotmail.com>
2023-01-19 22:53:22 -0300

    Forms: Screen Register

--- calendar/src/components/formRegister/form.js
+++ calendar/src/components/formRegister/form.js
@@ -1,23 +1,68 @@
-import './form.css'
+import "./form.css";
 
 function Registro() {
   return (
     <div className="formulario">
-      <h1>Welcome,</h1>
-      <p>Please, register to continue</p>
+      <div className="texts">
+        <h1>Welcome,</h1>
+        <p>Please, register to continue</p>
+      </div>
 
-      <input placeholder="Your first name"></input>
-      <input placeholder="Your last name"></input>
-      <input placeholder="YMM/DD/YYYY"></input>
-      <input placeholder="Your Country"></input>
-      <input placeholder="Your City"></input>
-      <input placeholder="A valid e-mail here"></input>
-      <input placeholder="Your password"></input>
-      <input placeholder="Confirm your password"></input>
+      <div className="forms">
+        <div>
+          {" "}
+          <label>
+            first name
+            <input type="text" placeholder="Your first name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            last name
+            <input placeholder="Your last name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            birth date
+            <input placeholder="YMM/DD/YYYY" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Country
+            <input placeholder="Your Country" />
+          </label>
+        </div>
+        <div>
+          <label>
+            City
+            <input placeholder="Your City" />
+          </label>
+        </div>
+        <div>
+          <label>
+            e-mail
+            <input placeholder="Valid e-mail here" />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input placeholder="Your password" />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input placeholder="Confirm your password" />
+          </label>
+        </div>
+      </div>
 
       <button>Register Now</button>
     </div>
   );
 }
 
-export default Registro
+export default Registro;
