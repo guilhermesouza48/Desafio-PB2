3d2574ae4c5f1061079456e0018154757b253d0e
Guilherme Souza de Jesus <guilhermesouzacrs@hotmail.com>
2023-01-21 11:39:43 -0300

    Correction: Name divs

--- calendar/src/components/formRegister/form.js
+++ calendar/src/components/formRegister/form.js
@@ -2,48 +2,48 @@ import "./form.css";
 
 function Registro() {
   return (
-    <div className="formulario">
+    <div className="main">
       <div className="texts">
         <h1>Welcome,</h1>
         <p>Please, register to continue</p>
       </div>
 
       <div className="forms">
-        <div className="label_corrent">
+        <div className="label_chain">
           <label>first name </label>
           <input type="text" placeholder="Your first name" />
         </div>
-        <div className="label_corrent">
+        <div className="label_chain">
           <label>last name</label>
           <input placeholder="Your last name" />
         </div>
-        <div className="label_corrent">
+        <div className="label_chain">
           <label>birth date</label>
           <input placeholder="YMM/DD/YYYY" />
         </div>
-        <div className="label_corrent">
+        <div className="label_chain">
           <label>Country</label>
           <input placeholder="Your Country" />
         </div>
-        <div className="label_corrent">
+        <div className="label_chain">
           <label>City </label>
           <input placeholder="Your City" />
         </div>
-        <div className="label_corrent">
+        <div className="label_chain">
           <label>e-mail</label>
           <input placeholder="Valid e-mail here" />
         </div>
-        <div className="label_corrent">
+        <div className="label_chain">
           <label>password</label>
           <input placeholder="Your password" />
         </div>
-        <div className="label_corrent">
+        <div className="label_chain">
           <label>password</label>
           <input placeholder="Confirm your password" />
         </div>
       </div>
 
-      <button>Register Now</button>
+      <button onclick="">Register Now</button>
     </div>
   );
 }
